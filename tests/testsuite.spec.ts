3cf0102658f772e0cb1b2d06a293de6180a11604
GitMit3 <148076024+GitMit3@users.noreply.github.com>
2024-09-30 17:30:03 +0200

    TC 01 Hämtar alla fakturor

---
+++ tests/testsuite.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect, APIRequestContext } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
+const BASE_URL = 'http://localhost:3000';
+
+test.describe('TheTester Hotel API Tests', () => {
+  let request: APIRequestContext;
+  let token: string;
+
+  test.beforeAll(async ({ playwright }) => {
+    request = await playwright.request.newContext({
+      baseURL: BASE_URL,
+    });
+
+
+    const loginResponse = await request.post('/api/login', {
+      data: {
+        username: 'tester01',
+        password: 'GteteqbQQgSr88SwNExUQv2ydb7xuf8c',
+      },
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const loginData = await loginResponse.json();
+    token = loginData.token;
+  });
+
+  // 1. Hämta alla fakturor
+  test('TC 01 - hämta alla fakturor', async () => {
+    const response = await request.get('/api/bills', {
+      headers: { 'X-user-auth': JSON.stringify({ username: 'tester01', token }) },
+    });
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+    const bills = await response.json();
+    expect(bills.length).toBeGreaterThan(0);
+  });
